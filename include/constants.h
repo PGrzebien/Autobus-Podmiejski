f961190c13eda878a68250d573893a53beaa9065
Patryk Grzebień <patryk.grzebien1@gmail.com>
2026-01-16 21:44:08 +0100

    feat: define simulation parameters N, P, R, T in constants.h

--- include/constants.h
+++ include/constants.h
@@ -3,8 +3,14 @@
 
 #include <sys/types.h>
 
-// Klucz dla pamięci dzielonej (musi być unikalny w systemie)
+// Klucze IPC
 #define SHM_KEY 123456
+
+// Parametry symulacji
+#define N_BUSES 3         // Liczba autobusów (N)
+#define P_CAPACITY 15     // Ogólna pojemność autobusu (P)
+#define R_BIKES 5         // Limit miejsc na rowery (R)
+#define T_WAIT 10         // Czas oczekiwania na przystanku (T)
 
 // Typy pasażerów
 typedef enum {
@@ -26,7 +32,7 @@ typedef struct {
     int is_at_station;
 } BusState;
 
-// Deklaracja funkcji narzędziowej (użyjemy jej w main)
+// Deklaracja funkcji narzędziowej
 void check_error(int result, const char* msg);
 
 #endif
